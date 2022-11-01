4010ef8d2eca5ae7ee7274d56c86b3eb4935eaf8
solssak <gswoong1012@gmail.com>
2022-11-02 01:16:55 +0900

    Start project

---
+++ assets/scripts/app.js
@@ -0,0 +1,42 @@
+// 상품 리스트 렌더링하고 버튼
+
+const productList = {
+  products: [
+    {
+      title: "A Pillow",
+      imageUrl: "https://fifabell.github.io/assets/images/oop.png",
+      price: 19.99,
+      description: "A soft pillow!",
+    },
+    {
+      title: "A Carpet",
+      imageUrl: "https://fifabell.github.io/assets/images/oop.png",
+      price: 89.99,
+      description: "A carpet which you might like - or not",
+    },
+  ],
+  render() {
+    const renderHook = document.getElementById("app");
+    const prodList = document.createElement("ul");
+    prodList.className = "product-list";
+    for (const prod of this.products) {
+      const prodEl = document.createElement("li");
+      prodEl.className = "product-item";
+      prodEl.innerHTML = `
+        <div>
+          <img src="${prod.imageUrl}" alt="${prod.title}" >
+          <div class="product-item__content">
+          <h2>${prod.title}</h2>
+            <h3>\${prod.price}</h3>
+            <p>${prod.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      `;
+      prodList.append(prodEl);
+    }
+    renderHook.append(prodList);
+  },
+};
+
+productList.render();
